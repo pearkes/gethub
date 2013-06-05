09a3707b4254dfcf0f88bf3b44b8302df17cbd22
Jack Pearkes <jackpearkes@gmail.com>
2013-06-05 11:24:27 +0200

    Move github to a step

--- steps/step_retrieve_repositories.go
+++ steps/step_retrieve_repositories.go
@@ -19,11 +19,54 @@ func (r Repo) Name() string {
 	return strings.Split(r.FullName, "/")[1]
 }
 
-type stepCheckPath struct{}
+type stepRetrieveRepositories struct{}
 
-func (*stepCheckPath) Run(state map[string]interface{}) multistep.StepAction {
-	// Do some stuff
+func (*stepRetrieveRepositories) Run(state map[string]interface{}) multistep.StepAction {
+	log.Println("Retrieving remote repositories...")
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET",
+		"https://api.github.com/user/repos?type=all&per_page=100", nil)
+
+	req.SetBasicAuth(state["token"], "")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return multistep.ActionHalt
+	}
+
+	var repos []Repo
+
+	err = json.Unmarshal(body, &repos)
+
+	if err != nil {
+		fmt.Println(err)
+		return multistep.ActionHalt
+	}
+
+	log.Println(resp.Status)
+
+	if resp.StatusCode != 200 {
+		fmt.Println(red + "Uh oh, there was an error getting your repositories from GitHub. Here's what we got back:\n" + clear)
+		fmt.Println(string(body))
+		return multistep.ActionHalt
+	}
+
+	log.Println(len(repos), "repositories retrieved from GitHub")
+
+	state["repos"] = repos
+
 	return multistep.ActionContinue
 }
 
-func (*stepCheckPath) Cleanup(map[string]interface{}) {}
+func (*stepRetrieveRepositories) Cleanup(map[string]interface{}) {}
